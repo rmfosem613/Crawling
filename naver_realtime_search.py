18cb79bdb7235ba922a95fc7281afb15c2fc6ab1
migyeong <rmfosem613@gmail.com>
2020-07-13 20:32:45 +0900

    Create naver_realtime_search.py

---
+++ naver_realtime_search.py
@@ -0,0 +1,16 @@
+import requests 
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
+url = 'https://datalab.naver.com/keyword/realtimeList.naver?where=main'
+res = requests.get(url, headers = headers)
+soup = BeautifulSoup(res.content, 'html.parser')
+data = soup.select('span.item_title')
+i=1
+f = open("네이버_실시간_순위.txt", "w")
+for item in data:
+    data = str(i) + "위 : " + item.get_text() + "\n"
+    i = i + 1
+    f.write(data)
+f.close()
